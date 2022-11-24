bbbb53a46f30e39b37260b1765319c7ad562080c
donthemon <abubakar.semasaba@hotmail.com>
2022-11-24 14:06:44 +0800

    Initial Commit
    Add all files for the project.

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload = function digital_fn() {
+  let toggle = document.querySelector("#nav .toggle-btn");
+  let collapse = document.querySelector("#nav .collapse");
+
+  toggle.addEventListener("click", function (event) {
+    collapse.classList.toggle("active");
+  });
+
+  //initialize masonry
+  let grid = document.querySelector(
+    "#site-main .recent-work-area .images-flex"
+  );
+  let msnry = new Masonry(grid, {
+    itemSelector: ".flex-item",
+    gutter: 100,
+    fitwidth: true,
+  });
+};
